a3c2cda431fb78986cc889f153e78ae53801ba22
Kevin Isom <kevin.isom@gmail.com>
2016-11-08 16:03:20 +1300

    migrate command

---
+++ commands/migrate.js
@@ -0,0 +1,35 @@
+const pluralize = require('pluralize');
+const to = require('to-case');
+const mkdirp = require('mkdirp');
+const timestamp = require('../utils/timestamp')();
+const writeFile = require('../utils/write-file');
+
+
+exports.command = 'migrate <prefix>'
+exports.desc = 'Generates a migration file'
+exports.builder = {
+  table: {
+    alias: 'n',
+    describe: 'The name of the table used in the migration',
+    demand: false
+  }
+}
+exports.handler = function (argv) {
+  const { prefix, name, surname } = argv
+  const migration = `
+exports.up = function(knex, Promise) {
+
+};
+
+exports.down = function(knex, Promise) {
+
+};
+`;
+  const sluggedname = to.slug(prefix);
+
+  const path = `./migrations/${timestamp}-${sluggedname}.js`;
+  mkdirp.sync(`${process.cwd()}/migrations/`);
+  writeFile(path, migration);
+  console.log(`created: ${path}`);
+
+}
